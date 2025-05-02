da4fd9260e0ea40de47bdabfffc2cde93852fb9a
Italo Souza Melo <italosm29@gmail.com>
2025-05-02 10:37:28 -0300

    refac: container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+
 import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Olá mundo!
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
 
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
-        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
-        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
-        quia rerum!
-      </p>
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
