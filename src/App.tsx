7f0f179644810c9d88b6afe402cc4b6a45726d2a
Italo Souza Melo <italosm29@gmail.com>
2025-05-06 16:51:58 -0300

    feat: add task context

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,28 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Home } from "./Pages/Home";
+import { TaskContextProvider } from "./contexts/TaskContext";
+
+// import { TaskStateModel } from "./models/TaskStateModel";
+// import { TaskContext } from "./contexts/TaskContext";
+
+// const initialState: TaskStateModel = {
+//   tasks: [],
+//   secondsRemaining: 0,
+//   formattedSecondsRemaining: "00:00",
+//   activeTask: null,
+//   currentCycle: 0,
+//   config: {
+//     workTime: 25,
+//     shortBreakTime: 5,
+//     longBreakTime: 15,
+//   },
+// };
 
 export function App() {
-  return <Home />;
+  return (
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
+  );
 }
