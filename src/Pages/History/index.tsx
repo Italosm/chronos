e6185480968b94b2197e04bb308d3f2fc81b8902
Italo Souza Melo <italosm29@gmail.com>
2025-05-15 23:44:09 -0300

    feat: delete history.

--- src/Pages/History/index.tsx
+++ src/Pages/History/index.tsx
@@ -8,60 +8,79 @@ import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { formatDate } from '../../utils/formatDate';
 import { getTaskStatus } from '../../utils/getTaskStatus';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function History() {
-  const { state } = useTaskContext();
+  const { state, dispatch } = useTaskContext();
+  const hasTasks = state.tasks.length > 0;
+  const sortedTasks = [...state.tasks].sort((a, b) => {
+    return b.startDate - a.startDate;
+  });
   const taskTypeDictionary = {
     workTime: 'Foco',
     shortBreakTime: 'Descanso curto',
     longBreakTime: 'Descanso longo',
   };
+  function handleResetHistory() {
+    if (!confirm('Certeza?')) return;
+    dispatch({ type: TaskActionTypes.RESET_STATE });
+  }
   return (
     <MainTemplate>
       <Container>
         <Heading>
-          <span>History</span>
-          <span className={styles.buttonContainer}>
-            <div className={styles.container}>
-              <Button
-                icon={<TrashIcon />}
-                color='red'
-                aria-label='Apagar todo o histórico'
-              />
-              <span className={styles.buttonLabel}>apagar histórico</span>
+          <span className={styles.container}>History</span>
+          {hasTasks && (
+            <div className={styles.buttonContainer}>
+              <div className={styles.container}>
+                <Button
+                  icon={<TrashIcon />}
+                  color='red'
+                  aria-label='Apagar todo o histórico'
+                  onClick={handleResetHistory}
+                />
+                <span className={styles.buttonLabel}>apagar histórico</span>
+              </div>
             </div>
-          </span>
+          )}
         </Heading>
       </Container>
 
       <Container>
-        <div className={styles.responsiveTable}>
-          <table>
-            <thead>
-              <tr>
-                <th>Tarefa</th>
-                <th>Duração</th>
-                <th>Data</th>
-                <th>Status</th>
-                <th>Tipo</th>
-              </tr>
-            </thead>
+        {!hasTasks && (
+          <p style={{ textAlign: 'center' }}>
+            Experimente iniciar uma nova tarefa.
+          </p>
+        )}
+        {hasTasks && (
+          <div className={styles.responsiveTable}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Tarefa</th>
+                  <th>Duração</th>
+                  <th>Data</th>
+                  <th>Status</th>
+                  <th>Tipo</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {state.tasks.map(task => {
-                return (
-                  <tr key={task.id}>
-                    <td>{task.name}</td>
-                    <td>{task.duration}min</td>
-                    <td>{formatDate(task.startDate)}</td>
-                    <td>{getTaskStatus(task, state.activeTask)}</td>
-                    <td>{taskTypeDictionary[task.type]}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+              <tbody>
+                {sortedTasks.map(task => {
+                  return (
+                    <tr key={task.id}>
+                      <td>{task.name}</td>
+                      <td>{task.duration}min</td>
+                      <td>{formatDate(task.startDate)}</td>
+                      <td>{getTaskStatus(task, state.activeTask)}</td>
+                      <td>{taskTypeDictionary[task.type]}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        )}
       </Container>
     </MainTemplate>
   );
